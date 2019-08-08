f4496ae0f8d811868a00093c14b622c6476d614c
Sir Louis <me@sirlouis.io>
2019-08-08 04:34:07 +0100

    minor fix

--- app.init.php
+++ app.init.php
@@ -1,8 +1,10 @@
 <?php
 // load up app settings
 $conf_path = \str_replace([
-  "/vendor/7os/web-app",
-  "/dev/7os/web-app",
+  "/vendor/7os/web-page-500",
+  "/dev/7os/web-page-500",
+  "\\vendor\\7os\\web-page-500",
+  "\\dev\\7os\\web-page-500",
 ],"",__DIR__) . "/.system/appdata/7os/web-project";
 $conf_file = $conf_path . "/app.config";
 
@@ -20,4 +22,4 @@ $base_include = $conf->baseInclude;
 if (!\file_exists($base_include)) {
   throw new \Exception("[\"{$base_include}\"]: does not exist, kindly revert to 7 OS Web - app manual.", 1);
 }
-\define('APP_BASE_INC',$base_include);
+\define('APP_BASE_INC',$base_include . "/inc.php");
