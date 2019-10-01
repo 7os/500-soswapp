0fe7566beaefc15f0aadce7f2a94a86b55871bf5
Sir Louis <me@sirlouis.io>
2019-10-01 16:00:16 +0100

    feature fix

--- index.php
+++ index.php
@@ -1,6 +1,13 @@
 <?php
+namespace TymFrontiers;
 require_once "app.init.php";
 require_once APP_BASE_INC;
+$gen = new Generic;
+$params = $gen->requestParam([
+  "request" => ["request","text",3,0],
+  "message" => ["message","text",3,0]
+],'get',[]);
+if (!empty($params['message'])) $params['message'] = \urldecode($params['message']);
 ?>
 <!DOCTYPE html>
 <html lang="en" dir="ltr" manifest="./site.manifest">
@@ -16,31 +23,49 @@ require_once APP_BASE_INC;
     <meta name="publisher" content="Your company">
     <meta name="robots" content='index'>
     <!-- Theming styles -->
-    <link rel="stylesheet" href="//cdn.tymfrontiers.net/get/7os/web-theme/web-theme.css">
-    <link rel="stylesheet" href="//cdn.tymfrontiers.net/get/7os/web-theme/web-theme-<?php echo PRJ_THEME; ?>.css">
+    <link rel="stylesheet" href="//cdn.tymapp.com/get/7os/web-theme/web-theme.css">
+    <link rel="stylesheet" href="//cdn.tymapp.com/get/7os/web-theme/web-theme-<?php echo PRJ_THEME; ?>.css">
     <!-- optional plugin -->
-    <link rel="stylesheet" href="//cdn.tymfrontiers.net/get/7os/web-plugin/web-plugin.css">
-    <link rel="stylesheet" href="//cdn.tymfrontiers.net/get/7os/web-drag-nav/web-drag-nav.css">
-    <link rel="stylesheet" href="//cdn.tymfrontiers.net/get/7os/web-fader-box/web-fader-box.css">
+    <link rel="stylesheet" href="//cdn.tymapp.com/get/7os/web-plugin/web-plugin.css">
+    <link rel="stylesheet" href="//cdn.tymapp.com/get/7os/web-drag-nav/web-drag-nav.css">
+    <link rel="stylesheet" href="//cdn.tymapp.com/get/7os/web-fader-box/web-fader-box.css">
     <!-- Project styling -->
     <link rel="stylesheet" href="<?php echo \html_style("base.css"); ?>">
   </head>
   <body>
     <?php include PRJ_INC_HEADER; ?>
     <section id="main-content">
-      <h1>500 Internal Server Error</h1>
-      <p>We are sorry its our fault</p>
+      <div class="view-space">
+        <div class="sec-div padding -p20">
+          <h1> <i class="fas fa-sad-tear fa-2x"></i> It's our fault.</h1>
+          <?php if (!empty($params['request'])): ?>
+            <blockquote>
+              <p><?php echo $params['request']; ?></p>
+            </blockquote>
+          <?php endif; ?>
+          <p>
+            <?php if (!empty($params["message"])){
+              echo $params['message'];
+            } else {
+              echo "There is a situation here now but we assure you services will be fully restored soon.";
+            } ?>
+          </p>
+          <p> <i class="fas fa-angle-double-left"></i> <a href="<?php echo WHOST; ?>"><b>Go back home</b></a> </p>
+          <br class="c-f">
+        </div>
+
+      </div>
     </section>
     <?php include PRJ_INC_FOOTER; ?>
     <!-- Required scripts -->
-    <script src="//cdn.tymfrontiers.net/get/jquery/jquery/jquery.min.js">  </script>
-    <script src="//cdn.tymfrontiers.net/get/7os/js-generic-fn/js-generic-fn.min.js">  </script>
-    <script src="//cdn.tymfrontiers.net/get/7os/web-theme/web-theme.min.js" charset="utf-8"></script>
+    <script src="//cdn.tymapp.com/get/jquery/jquery/jquery.min.js">  </script>
+    <script src="//cdn.tymapp.com/get/7os/js-generic-fn/js-generic-fn.min.js">  </script>
+    <script src="//cdn.tymapp.com/get/7os/web-theme/web-theme.min.js" ></script>
     <!-- optional plugins -->
-    <script src="//cdn.tymfrontiers.net/get/7os/web-plugin/web-plugin.min.js" charset="utf-8"></script>
-    <script src="//cdn.tymfrontiers.net/get/7os/web-drag-nav/web-drag-nav.min.js" charset="utf-8"></script>
-    <script src="//cdn.tymfrontiers.net/get/7os/web-fader-box/web-fader-box.min.js" charset="utf-8"></script>
+    <script src="//cdn.tymapp.com/get/7os/web-plugin/web-plugin.min.js" ></script>
+    <script src="//cdn.tymapp.com/get/7os/web-drag-nav/web-drag-nav.min.js" ></script>
+    <script src="//cdn.tymapp.com/get/7os/web-fader-box/web-fader-box.min.js" ></script>
     <!-- project scripts -->
-    <script src="<?php echo \html_script ("base.min.js"); ?>" charset="utf-8"></script>
+    <script src="<?php echo \html_script ("base.min.js"); ?>" ></script>
   </body>
 </html>
